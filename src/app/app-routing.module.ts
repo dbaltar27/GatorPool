8b4eee2efeaffd285af3463431b74121df30b18f
Andrew <andrewdempsey@ufl.edu>
2019-03-16 18:50:43 -0400

    first

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,9 +3,17 @@ import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 
 import { ProfileComponent } from './profile/profile.component';
+import { HomeComponent } from './home/home.component';
+import { TripFormComponent } from './trip-form/trip-form.component';
+import { DriverFormComponent } from './driver-form/driver-form.component';
+import { SignupComponent } from './signup/signup.component';
 
 const routes: Routes = [
+  { path: '', component: HomeComponent },
   { path: 'profile', component: ProfileComponent },
+  { path: 'new-trip', component: TripFormComponent },
+  { path: 'new-driver', component: DriverFormComponent },
+  { path: 'signup', component: SignupComponent },
 ]
 
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,20 @@ import { AppComponent } from './app.component';
 import { ProfileComponent } from './profile/profile.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { AppRoutingModule } from './app-routing.module';
+import { HomeComponent } from './home/home.component';
+import { TripFormComponent } from './trip-form/trip-form.component';
+import { DriverFormComponent } from './driver-form/driver-form.component';
+import { SignupComponent } from './signup/signup.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProfileComponent,
-    NavbarComponent
+    NavbarComponent,
+    HomeComponent,
+    TripFormComponent,
+    DriverFormComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
