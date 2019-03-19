d2473ad1fa259d6572b83ff898069ed39be2bae2
dbaltar27 <dbaltar27@ufl.edu>
2019-03-19 19:03:38 -0400

    Rating form update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 
 import { ProfileComponent } from './profile/profile.component';
+import { RatingFormComponent } from './rating-form/rating-form.component'
 
 const routes: Routes = [
   { path: 'profile', component: ProfileComponent },
+  { path: 'rating', component: RatingFormComponent },
 ]
-
 
 @NgModule({
   imports: [
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppComponent } from './app.component';
 import { ProfileComponent } from './profile/profile.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { AppRoutingModule } from './app-routing.module';
+import { RatingFormComponent } from './rating-form/rating-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProfileComponent,
-    NavbarComponent
+    NavbarComponent,
+    RatingFormComponent
   ],
   imports: [
     BrowserModule,
