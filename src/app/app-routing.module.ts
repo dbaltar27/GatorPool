413a2ccd7a46a7acdd37e29f45dbe2c52c2281b3
dbaltar27 <dbaltar27@ufl.edu>
2019-04-02 20:23:33 -0400

    ratings updated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,10 +4,12 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { ProfileComponent } from './profile/profile.component';
 import { RatingFormComponent } from './rating-form/rating-form.component'
+import { DetailsPageComponent } from './details-page/details-page.component';
 
 const routes: Routes = [
   { path: 'profile', component: ProfileComponent },
   { path: 'rating', component: RatingFormComponent },
+  { path: 'details', component: DetailsPageComponent }
 ]
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { ProfileComponent } from './profile/profile.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { AppRoutingModule } from './app-routing.module';
 import { RatingFormComponent } from './rating-form/rating-form.component';
+import { DetailsPageComponent } from './details-page/details-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProfileComponent,
     NavbarComponent,
-    RatingFormComponent
+    RatingFormComponent,
+    DetailsPageComponent
   ],
   imports: [
     BrowserModule,
