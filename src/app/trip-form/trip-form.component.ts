241ba7f94377d736904a992498f2e55beb90429d
ffore <daniel.geleroff@gmail.com>
2019-04-03 10:12:54 -0400

    grabbing drive details into session storage

--- src/app/trip-form/trip-form.component.ts
+++ src/app/trip-form/trip-form.component.ts
@@ -7,6 +7,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TripFormComponent implements OnInit {
 
+	
+	// value='';
+	onClickMe(city: string, state: string, date: string){
+		// this.value = value;
+		console.log("city " + city);
+		console.log("state " + state);
+		console.log("date" + date);
+		var drive = {
+			dest_city: city,
+			dest_state: state,
+			ride_date: date
+		};
+
+
+		sessionStorage.setItem("drive", JSON.stringify(drive));
+	}
+
+	
+
   constructor() { }
 
   ngOnInit() {
@@ -14,6 +33,26 @@ export class TripFormComponent implements OnInit {
     //(<any>$('.date') ).datepicker({
       //autoclose: true,
     //});
+
+    // $(document).ready(function(){
+
+    window.onload = function(){
+    	$("#driver_btn").click(function(){
+
+	    	var x = document.getElementById("tester");
+
+	    	console.log(x);
+
+
+
+	    	// console.log("city: " + $("#dest_city").val());
+
+	    });
+    // });
+	
+   }
+	    
+
   }
 
 }
