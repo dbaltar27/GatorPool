b8ffdda5b8c53dde7999fc2dd36036f8d6cd16d0
alexblauth <thomas98alex@live.com>
2019-04-04 12:17:20 -0400

    Updated rating form

--- src/app/rating-form/rating-form.component.ts
+++ src/app/rating-form/rating-form.component.ts
@@ -8,6 +8,13 @@ import { Component, OnInit } from '@angular/core';
 export class RatingFormComponent implements OnInit {
 
   constructor() { }
+  
+
+
+  trip_driver = sessionStorage.getItem("driver");
+  other_rider_1 = sessionStorage.getItem("rider1");
+  other_rider_2 = sessionStorage.getItem("rider2");
+  trip_date = sessionStorage.getItem("date");
 
   ngOnInit() {
   }
