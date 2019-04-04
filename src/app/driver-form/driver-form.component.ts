6fbf48e33efbfb6f11dc51557c2dbc03e277f851
ffore <daniel.geleroff@gmail.com>
2019-04-04 10:53:11 -0400

    posting a drive and having it show on driving trips on profile

--- src/app/driver-form/driver-form.component.ts
+++ src/app/driver-form/driver-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ViewChild, ElementRef, OnInit } from '@angular/core';
 import {Location} from '@angular/common';
 
 @Component({
@@ -7,6 +7,47 @@ import {Location} from '@angular/common';
   styleUrls: ['./driver-form.component.css']
 })
 export class DriverFormComponent implements OnInit {
+
+
+	@ViewChild('am') AM: ElementRef;
+		funAM(){
+			this.AM.nativeElement.checked
+		}
+
+	@ViewChild('pm') PM: ElementRef;
+		funPM(){
+			this.PM.nativeElement.checked
+		}
+
+	onClickMe(hour: string, minute: string, seats: string){
+		console.log("hour: " + hour);
+		console.log("minute: " + minute);
+		
+		var am_pm;
+
+		if(this.AM.nativeElement.checked){
+			console.log("AM");
+			am_pm = "am";
+		}
+		else{
+			console.log("PM");
+			am_pm = "pm";
+		}
+
+		var userTime = hour + ":" + minute + "" + am_pm;
+
+		// var drive_details = {
+		// 	time: userTime
+		// };
+
+		var drive = JSON.parse(sessionStorage.getItem("drive"));
+		sessionStorage.removeItem("drive");
+
+		drive["time"] = userTime;
+		drive["seats"] = seats;
+		sessionStorage.setItem("drive", JSON.stringify(drive));
+
+	};
 
   constructor(private _location: Location) { }
   
--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-profile',
@@ -7,9 +7,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileComponent implements OnInit {
 
-  constructor() { }
+	onClickMe(){
+		console.log("blah");
+	}
+
+  constructor(private elementRef: ElementRef) { }
+
+  ngAfterViewInit(){
+  	this.elementRef.nativeElement.querySelector('my-element').addEventListener('click', this.onClick.bind(this));
+  }
+
+  onClick(event){
+  	console.log();
+  }
 
   ngOnInit() {
+  	console.log("getting drive");
+  	var sessionDrive = JSON.parse(sessionStorage.getItem("drive"));
+  	
+
+  	if(sessionDrive){
+  		console.log(sessionDrive);
+  		$('#driving-table tr:last').after('<tr><td>'
+  			+sessionDrive.ride_date+'</td><td>Gainesville</td><td>'
+  			+sessionDrive.dest_city+'</td><td>2/'+sessionDrive.seats
+  			+'</td><td><button (click)="onClickMe()" class="btn btn-default" routerLinkActive="active" routerLink="/trip-page">View</button></td></tr>');
+  		
+  	}
+  	else{
+  		console.log("no session storage found");
+  	}
+
+
+
+  	
   }
 
 }
+
